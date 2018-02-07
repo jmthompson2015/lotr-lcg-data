e3273234373602f014b6f02fd46b8c84f399154e
jmthompson2015 <softwaredev@jeffreythompson.net>
2018-02-07 13:48:19 -0700

    Changed to work for Quest cards.

--- src/js/HoBDetailFetcher.js
+++ src/js/HoBDetailFetcher.js
@@ -29,57 +29,43 @@ define(["js/FileLoader"], function(FileLoader)
       var key3 = "<div class=\"statTextBox\"";
       var key4 = "<div class=\"extraBox\"";
 
-      // Process the title name box.
       var index0 = content.indexOf(key0);
       var index1 = content.indexOf(key1);
-      var fragment0 = content.substring(index0, index1);
-      var data0 = processTitleBox(fragment0);
+      var titleBoxFragment = content.substring(index0, index1);
 
-      // Process the stat box.
       var index2 = content.indexOf(key2);
-      var fragment1 = content.substring(index1, index2);
-      var data1 = processStatBox(fragment1);
+      var statBoxFragment = content.substring(index1, index2);
 
-      // Process the stat value box.
       var index3 = content.indexOf(key3);
-      var fragment2 = content.substring(index2, index3);
-      var data2 = processStatValueBox(fragment2);
+      var statValueBoxFragment = content.substring(index2, index3);
 
-      // Process the stat text box.
       var index4 = content.indexOf(key4);
-      var fragment3 = content.substring(index3, index4);
-      var data3 = processStatTextBox(fragment3);
+      var statTextBoxFragment = content.substring(index3, index4);
 
-      var detail = {
-         "pack_name": data0.pack_name,
-         "encounter_set": data1.encounter_set,
-         "type_code": data1.type_code,
-         "type_name": data1.type_name,
-         "position": data0.position,
-         "name": data0.name,
-      };
+      var statBoxData = processStatBox(statBoxFragment);
+      var type_code = (Array.isArray(statBoxData) ? statBoxData[0].type_code : statBoxData.type_code);
+      var titleBoxData = processTitleBox(titleBoxFragment, type_code);
+      var statValueBoxData = processStatValueBox(statValueBoxFragment, type_code);
+      var statTextBoxData = processStatTextBox(statTextBoxFragment, type_code);
+      var detail;
 
-      maybeAddData(detail, "is_unique", data0.is_unique);
-      maybeAddData(detail, "traits", data3.traits);
-      maybeAddData(detail, "text", data3.text);
-      maybeAddData(detail, "shadow", data3.shadow);
-      maybeAddData(detail, "engagement_cost", data2.engagement_cost);
-      maybeAddData(detail, "threat", data2.threat);
-      maybeAddData(detail, "attack", data2.attack);
-      maybeAddData(detail, "defense", data2.defense);
-      maybeAddData(detail, "hit_points", data2.hit_points);
-      maybeAddData(detail, "quest_points", data2.quest_points);
-      maybeAddData(detail, "quantity_easy", data0.quantity_easy);
-      maybeAddData(detail, "quantity", data0.quantity);
-
-      if (data0.imageFile !== undefined)
+      if (type_code === "quest")
       {
-         var image = "encounter-card/" + data1.encounter_set.replace(/\"/g, "") + "/" + data0.imageFile;
-         console.log("image = " + image);
-         detail.image = image;
-      }
+         var details = [];
 
-      callback(detail);
+         for (var i = 0; i < 2; i++)
+         {
+            detail = createDetailObject(titleBoxData[i], statBoxData[i], statTextBoxData[i], statValueBoxData[i]);
+            details.push(detail);
+         }
+
+         callback(details);
+      }
+      else
+      {
+         detail = createDetailObject(titleBoxData, statBoxData, statTextBoxData, statValueBoxData);
+         callback(detail);
+      }
    };
 
    function processStatBox(fragment)
@@ -96,20 +82,46 @@ define(["js/FileLoader"], function(FileLoader)
       var encounter_set = extractAttribute(fragment1, "title");
       console.log("encounter_set = " + encounter_set);
 
-      return (
+      var encounter_sets;
+      if (type_code === "quest")
       {
+         var parts = fragment.split("<a title=\"");
+         encounter_sets = [];
+
+         for (var i = 2; i < parts.length; i++)
+         {
+            var part = parts[i];
+            encounter_sets.push(part.substring(0, part.indexOf("\"")));
+         }
+
+         console.log("encounter_sets = " + encounter_sets);
+      }
+
+      var data0 = {
          "encounter_set": encounter_set,
          "type_code": type_code,
          "type_name": type_name,
-      });
+         "encounter_sets": encounter_sets,
+      };
+
+      var data1 = {
+         "encounter_set": encounter_set,
+         "type_code": type_code,
+         "type_name": type_name,
+         "encounter_sets": encounter_sets,
+      };
+
+      return (type_code === "quest" ? [data0, data1] : data0);
    }
 
-   function processStatTextBox(fragment)
+   function processStatTextBox(fragment, type_code)
    {
       // console.log("processStatTextBox() fragment = " + fragment);
 
       var key1 = "<p class='main-text'>";
       var key2 = "<p class='shadow-text'>";
+      var key3 = "<p class='flavor-text'>";
+      var key4 = "<div class='victory-text'>";
       var index1 = fragment.indexOf(key1);
 
       if (index1 < 0)
@@ -143,34 +155,82 @@ define(["js/FileLoader"], function(FileLoader)
       fragment2 = extractBetween(fragment, key1, key2, undefined, true);
       if (fragment2 === undefined || fragment2.length === 0)
       {
+         fragment2 = extractBetween(fragment, key1, key3, undefined, true);
+      }
+      if (fragment2 === undefined || fragment2.length === 0)
+      {
+         fragment2 = extractBetween(fragment, key1, key4, undefined, true);
+      }
+      if (fragment2 === undefined || fragment2.length === 0)
+      {
          fragment2 = extractBetween(fragment, key1, "</div>", undefined, true);
       }
 
       var text = (fragment2 !== undefined ? extractContent(fragment2) : undefined);
       text = text.replace(/<\/p>/g, "");
       text = text.replace(/<p>/g, "<br/>");
-      text = (text !== undefined ? removeAnchor(text) : undefined);
-      text = (text !== undefined ? removeAnchor(text) : undefined);
-      text = text.trim();
+
+      while (text !== undefined && text.indexOf("<a") >= 0)
+      {
+         text = (text !== undefined ? removeAnchor(text) : undefined);
+      }
+
+      if (text !== undefined)
+      {
+         text = text.trim();
+      }
       console.log("text = " + text);
+
+      fragment2 = extractBetween(fragment, key3, "</p>", undefined, true);
+      if (fragment2 === undefined || fragment2.length === 0)
+      {
+         fragment2 = extractBetween(fragment, key3, key4, undefined, true);
+      }
+      if (fragment2 === undefined || fragment2.length === 0)
+      {
+         fragment2 = extractBetween(fragment, key3, "</div>", undefined, true);
+      }
+      var flavor = (fragment2 !== undefined ? extractContent(fragment2) : undefined);
+      if (flavor !== undefined)
+      {
+         flavor = flavor.trim();
+         if (flavor.endsWith("</p>"))
+         {
+            flavor = flavor.substring(0, flavor.length - "</p>".length);
+         }
+      }
+      console.log("flavor = " + flavor);
 
       fragment2 = extractBetween(fragment, key2, "</p>", undefined, true);
       var shadow = (fragment2 !== undefined ? extractContent(fragment2) : undefined);
       console.log("shadow = " + shadow);
 
-      return (
+      var data1;
+
+      if (type_code === "quest")
       {
+         index1 = fragment.indexOf("<div class=\"statTextBox\"", 1);
+         var fragment1 = fragment.substring(index1);
+         fragment1 = fragment1.replace("<p>", "<p class='main-text'>");
+
+         data1 = processStatTextBox(fragment1, "enemy");
+      }
+
+      var data0 = {
          "traits": traits,
          "text": text,
          "shadow": shadow,
-      });
+         "flavor": flavor,
+      };
+
+      return (type_code === "quest" ? [data0, data1] : data0);
    }
 
-   function processStatValueBox(fragment)
+   function processStatValueBox(fragment, type_code)
    {
       // console.log("processStatValueBox() fragment = " + fragment);
 
-      var engagement_cost, threat, attack, defense, hit_points, quest_points;
+      var engagement_cost, threat, attack, defense, hit_points, quest_points, quest_points1;
       var parts = fragment.split(">");
       parts.forEach(function(part, i)
       {
@@ -180,14 +240,31 @@ define(["js/FileLoader"], function(FileLoader)
          {
             if (i - 2 >= 0)
             {
-               if (threat !== undefined)
+               if (type_code === "quest")
+               {
+                  quest_points1 = parseInt(parts[i - 2].substring(0, parts[i - 2].indexOf("<")));
+                  if (Number.isNaN(quest_points1))
+                  {
+                     quest_points1 = undefined;
+                  }
+                  console.log("quest_points1 = " + quest_points1);
+               }
+               else if (threat !== undefined)
                {
                   quest_points = parseInt(parts[i - 2].substring(0, parts[i - 2].indexOf("<")));
+                  if (Number.isNaN(quest_points))
+                  {
+                     quest_points = undefined;
+                  }
                   console.log("quest_points = " + quest_points);
                }
                else
                {
                   engagement_cost = parseInt(parts[i - 2].substring(0, parts[i - 2].indexOf("<")));
+                  if (Number.isNaN(engagement_cost))
+                  {
+                     engagement_cost = undefined;
+                  }
                   console.log("engagement_cost = " + engagement_cost);
                }
             }
@@ -214,18 +291,23 @@ define(["js/FileLoader"], function(FileLoader)
          }
       });
 
-      return (
-      {
+      var data0 = {
          "engagement_cost": engagement_cost,
          "threat": threat,
          "attack": attack,
          "defense": defense,
          "hit_points": hit_points,
          "quest_points": quest_points,
-      });
+      };
+
+      var data1 = {
+         "quest_points": quest_points1,
+      };
+
+      return (type_code === "quest" ? [data0, data1] : data0);
    }
 
-   function processTitleBox(fragment)
+   function processTitleBox(fragment, type_code)
    {
       // console.log("processTitleBox() fragment = " + fragment);
 
@@ -236,6 +318,20 @@ define(["js/FileLoader"], function(FileLoader)
       var index2 = fragment.indexOf(endKey, index + 1);
       var fragment1 = fragment.substring(index, index2 + endKey.length);
       var name = extractContent(fragment1);
+      var name1;
+      if (type_code === "quest")
+      {
+         index = fragment.indexOf(startKey, index + 1);
+         var index22 = fragment.indexOf(endKey, index + 1);
+         fragment1 = fragment.substring(index, index22 + endKey.length);
+         name1 = extractContent(fragment1);
+         if (name1 === undefined || name1.indexOf("<") >= 0)
+         {
+            name1 = name;
+         }
+
+         console.log("name1 = " + name1);
+      }
       var is_unique = false;
 
       if (name.indexOf("unique-card.png") >= 0)
@@ -263,6 +359,7 @@ define(["js/FileLoader"], function(FileLoader)
       var quantities = extractBetween(positionQuantities, "(", ")");
       var quantity_easy = 0;
       var quantity = 0;
+      console.log("quantities = " + quantities);
 
       if (quantities.indexOf("/") >= 0)
       {
@@ -272,7 +369,7 @@ define(["js/FileLoader"], function(FileLoader)
       }
       else
       {
-         quantity_easy = quantities.substring(1);
+         quantity_easy = parseInt(quantities.substring(1));
       }
 
       console.log("quantity_easy = " + quantity_easy);
@@ -283,16 +380,87 @@ define(["js/FileLoader"], function(FileLoader)
       var imageFile = src.substring(src.lastIndexOf("/") + 1);
       console.log("imageFile = " + imageFile);
 
-      return (
+      var imageFile1;
+
+      if (type_code === "quest")
       {
+         index2 += fragment1.length;
+         fragment1 = extractBetween(fragment, "<img", "/>", index2, true);
+         src = extractAttribute(fragment1, "src");
+         imageFile1 = src.substring(src.lastIndexOf("/") + 1);
+         console.log("imageFile1 = " + imageFile1);
+      }
+
+      var sequence, sequence1;
+
+      if (type_code === "quest")
+      {
+         sequence = imageFile.substring(imageFile.lastIndexOf("-") + 1, imageFile.lastIndexOf("."));
+         console.log("sequence = " + sequence);
+         sequence1 = imageFile1.substring(imageFile1.lastIndexOf("-") + 1, imageFile1.lastIndexOf("."));
+         console.log("sequence1 = " + sequence1);
+      }
+
+      var data0 = {
          "pack_name": pack_name,
          "position": position,
          "name": name,
          "is_unique": is_unique,
+         "sequence": sequence,
          "quantity_easy": quantity_easy,
          "quantity": quantity,
          "imageFile": imageFile,
-      });
+      };
+
+      var data1 = {
+         "pack_name": pack_name,
+         "position": position,
+         "name": name1,
+         "sequence": sequence1,
+         "quantity_easy": 1,
+         "quantity": 0,
+         "imageFile": imageFile1,
+      };
+
+      return (type_code === "quest" ? [data0, data1] : data0);
+   }
+
+   function createDetailObject(titleBoxData, statBoxData, statTextBoxData, statValueBoxData)
+   {
+      var detail = {};
+
+      maybeAddData(detail, "pack_name", titleBoxData.pack_name);
+      maybeAddData(detail, "encounter_set", statBoxData.encounter_set);
+      maybeAddData(detail, "type_code", statBoxData.type_code);
+      maybeAddData(detail, "type_name", statBoxData.type_name);
+      maybeAddData(detail, "position", titleBoxData.position);
+      maybeAddData(detail, "name", titleBoxData.name);
+      maybeAddData(detail, "is_unique", titleBoxData.is_unique);
+      maybeAddData(detail, "sequence", titleBoxData.sequence);
+      maybeAddData(detail, "traits", statTextBoxData.traits);
+      maybeAddData(detail, "text", statTextBoxData.text);
+      maybeAddData(detail, "shadow", statTextBoxData.shadow);
+      maybeAddData(detail, "flavor", statTextBoxData.flavor);
+      maybeAddData(detail, "engagement_cost", statValueBoxData.engagement_cost);
+      maybeAddData(detail, "threat", statValueBoxData.threat);
+      maybeAddData(detail, "attack", statValueBoxData.attack);
+      maybeAddData(detail, "defense", statValueBoxData.defense);
+      maybeAddData(detail, "hit_points", statValueBoxData.hit_points);
+      maybeAddData(detail, "quest_points", statValueBoxData.quest_points);
+      maybeAddData(detail, "encounter_sets", statBoxData.encounter_sets);
+      maybeAddData(detail, "quantity_easy", titleBoxData.quantity_easy);
+      maybeAddData(detail, "quantity", titleBoxData.quantity);
+
+      if (titleBoxData.imageFile !== undefined)
+      {
+         var type_code = statBoxData.type_code;
+         var prefix = (type_code === "quest" ? "quest-card/" : "encounter-card/");
+         var image = prefix + statBoxData.encounter_set + "/" + titleBoxData.imageFile;
+         console.log("image = " + image);
+         detail.image = image;
+      }
+
+      return detail;
    }
 
    function extractAttribute(anchor, attributeName)
--- test/js/HoBDetailFetcherTest.js
+++ test/js/HoBDetailFetcherTest.js
@@ -16,7 +16,6 @@ define(["qunit", "js/HoBDetailFetcher"], function(QUnit, HoBDetailFetcher)
          // console.log("Enemy detail: " + JSON.stringify(detail));
 
          assert.equal(detail.pack_name, "Khazad-d&#251;m", "pack_name");
-         assert.equal(detail.scenario_name, undefined, "scenario_name");
          assert.equal(detail.encounter_set, "Into the Pit", "encounter_set");
          assert.equal(detail.type_code, "enemy", "type_code");
          assert.equal(detail.type_name, "Enemy", "type_name");
@@ -60,7 +59,6 @@ define(["qunit", "js/HoBDetailFetcher"], function(QUnit, HoBDetailFetcher)
          // console.log("Location detail: " + JSON.stringify(detail));
 
          assert.equal(detail.pack_name, "Khazad-d&#251;m", "pack_name");
-         assert.equal(detail.scenario_name, undefined, "scenario_name");
          assert.equal(detail.encounter_set, "Into the Pit", "encounter_set");
          assert.equal(detail.type_code, "location", "type_code");
          assert.equal(detail.type_name, "Location", "type_name");
@@ -104,7 +102,6 @@ define(["qunit", "js/HoBDetailFetcher"], function(QUnit, HoBDetailFetcher)
          // console.log("Objective detail: " + JSON.stringify(detail));
 
          assert.equal(detail.pack_name, "Khazad-d&#251;m", "pack_name");
-         assert.equal(detail.scenario_name, undefined, "scenario_name");
          assert.equal(detail.encounter_set, "The Seventh Level", "encounter_set");
          assert.equal(detail.type_code, "objective", "type_code");
          assert.equal(detail.type_name, "Objective", "type_name");
@@ -136,6 +133,174 @@ define(["qunit", "js/HoBDetailFetcher"], function(QUnit, HoBDetailFetcher)
       HoBDetailFetcher.fetch(file, callback);
    });
 
+   QUnit.test("fetch() Quest Entering the Mines", function(assert)
+   {
+      // Setup.
+      var file = "http://hallofbeorn.com/LotR/Details/Entering-the-Mines-KD";
+      var callback = function(details)
+      {
+         // Verify.
+         assert.ok(true, "test resumed from async operation");
+         assert.ok(details);
+         assert.equal(Array.isArray(details), true);
+         assert.equal(details.length, 2);
+         // console.log("Quest detail A: " + JSON.stringify(details[0]));
+
+         var detail = details[0];
+         assert.equal(detail.pack_name, "Khazad-d&#251;m", "pack_name");
+         assert.equal(detail.encounter_set, "Into the Pit", "encounter_set");
+         assert.equal(detail.type_code, "quest", "type_code");
+         assert.equal(detail.type_name, "Quest", "type_name");
+         assert.equal(detail.position, 64, "position");
+         assert.equal(detail.name, "Entering the Mines", "name");
+         assert.equal(detail.is_unique, undefined, "is_unique");
+         assert.equal(detail.sequence, "1A", "sequence");
+         assert.equal(detail.traits, undefined, "traits");
+         assert.equal(detail.text, "<b>Setup:</b> Search the encounter deck for East-gate and Cave Torch. Put East-gate into play as the active location, and have the first player attach Cave Torch to a hero of his choice. Set First Hall and Bridge of Khazad-dûm aside, out of play. Shuffle the encounter deck.", "text");
+         assert.equal(detail.shadow, undefined, "shadow");
+         assert.equal(detail.flavor, "You have been sent by the White Council to Moria, to deliver a message to Balin and his Dwarven colony. No one has heard from him in a while.", "flavor");
+         assert.equal(detail.engagement_cost, undefined, "engagement_cost");
+         assert.equal(detail.threat, undefined, "threat");
+         assert.equal(detail.attack, undefined, "attack");
+         assert.equal(detail.defense, undefined, "defense");
+         assert.equal(detail.hit_points, undefined, "hit_points");
+         assert.equal(detail.quest_points, undefined, "quest_points");
+         assert.equal(detail.victory, undefined, "victory");
+         assert.equal(detail.quantity_easy, 1, "quantity_easy");
+         assert.equal(detail.quantity, 0, "quantity");
+         assert.equal(detail.image, "quest-card/Into the Pit/Entering-the-Mines-1A.jpg", "image");
+
+         var encounter_sets = ["Twists and Turns", "Hazards of the Pit", "Goblins of the Deep"];
+         assert.equal(detail.encounter_sets.length, encounter_sets.length, "encounter_sets.length");
+         var i;
+         for (i = 0; i < encounter_sets.length; i++)
+         {
+            assert.equal(detail.encounter_sets[i], encounter_sets[i], "encounter_sets[" + i + "]");
+         }
+
+         // console.log("Quest detail B: " + JSON.stringify(details[1]));
+         detail = details[1];
+         assert.equal(detail.pack_name, "Khazad-d&#251;m", "pack_name");
+         assert.equal(detail.encounter_set, "Into the Pit", "encounter_set");
+         assert.equal(detail.type_code, "quest", "type_code");
+         assert.equal(detail.type_name, "Quest", "type_name");
+         assert.equal(detail.position, 64, "position");
+         assert.equal(detail.name, "Entering the Mines", "name");
+         assert.equal(detail.is_unique, undefined, "is_unique");
+         assert.equal(detail.sequence, "1B", "sequence");
+         assert.equal(detail.traits, undefined, "traits");
+         assert.equal(detail.text, "<b>When Revealed:</b> Reveal 1 encounter card per player, and add it to the staging area.<br/>Player cannot advance to the next stage of the scenario unless Bridge of Khazad-dûm is in their victory display.", "text");
+         assert.equal(detail.shadow, undefined, "shadow");
+         assert.equal(detail.flavor, "The doors of the East&ndash;gate hang crooked on their henges. The darkness inside the doorway is still and impenetrable, shutting out the last beams of a sinking sun.", "flavor");
+         assert.equal(detail.engagement_cost, undefined, "engagement_cost");
+         assert.equal(detail.threat, undefined, "threat");
+         assert.equal(detail.attack, undefined, "attack");
+         assert.equal(detail.defense, undefined, "defense");
+         assert.equal(detail.hit_points, undefined, "hit_points");
+         // quest_points missing
+         assert.equal(detail.victory, undefined, "victory");
+         assert.equal(detail.quantity_easy, 1, "quantity_easy");
+         assert.equal(detail.quantity, 0, "quantity");
+         assert.equal(detail.image, "quest-card/Into the Pit/Entering-the-Mines-1B.jpg", "image");
+
+         assert.equal(detail.encounter_sets.length, encounter_sets.length, "encounter_sets.length");
+         for (i = 0; i < encounter_sets.length; i++)
+         {
+            assert.equal(detail.encounter_sets[i], encounter_sets[i], "encounter_sets[" + i + "]");
+         }
+
+         done();
+      };
+
+      // Run.
+      var done = assert.async();
+      HoBDetailFetcher.fetch(file, callback);
+   });
+
+   QUnit.test("fetch() Quest Search for an Exit", function(assert)
+   {
+      // Setup.
+      var file = "http://hallofbeorn.com/LotR/Details/Search-for-an-Exit-Heading-Down-KD";
+      var callback = function(details)
+      {
+         // Verify.
+         assert.ok(true, "test resumed from async operation");
+         assert.ok(details);
+         assert.equal(Array.isArray(details), true);
+         assert.equal(details.length, 2);
+         // console.log("Quest detail A: " + JSON.stringify(details[0]));
+
+         var detail = details[0];
+         assert.equal(detail.pack_name, "Khazad-d&#251;m", "pack_name");
+         assert.equal(detail.encounter_set, "Flight from Moria", "encounter_set");
+         assert.equal(detail.type_code, "quest", "type_code");
+         assert.equal(detail.type_name, "Quest", "type_name");
+         assert.equal(detail.position, 70, "position");
+         assert.equal(detail.name, "Search for an Exit", "name");
+         assert.equal(detail.is_unique, undefined, "is_unique");
+         assert.equal(detail.sequence, "2A", "sequence");
+         assert.equal(detail.traits, undefined, "traits");
+         assert.equal(detail.text, "While Search for an Exit is the active quest card, only flip it to side 2B at the beginning of the staging step.", "text");
+         assert.equal(detail.shadow, undefined, "shadow");
+         assert.equal(detail.flavor, "As the presence draws near, doubt and fear surround you like a vast shadow. You must find daylight, you must escape from the Black Pit...", "flavor");
+         assert.equal(detail.engagement_cost, undefined, "engagement_cost");
+         assert.equal(detail.threat, undefined, "threat");
+         assert.equal(detail.attack, undefined, "attack");
+         assert.equal(detail.defense, undefined, "defense");
+         assert.equal(detail.hit_points, undefined, "hit_points");
+         assert.equal(detail.quest_points, undefined, "quest_points");
+         assert.equal(detail.victory, undefined, "victory");
+         assert.equal(detail.quantity_easy, 1, "quantity_easy");
+         assert.equal(detail.quantity, 0, "quantity");
+         assert.equal(detail.image, "quest-card/Flight from Moria/Search-for-an-Exit-Heading-Down-2A.jpg", "image");
+
+         var encounter_sets = ["Hazards of the Pit", "Deeps of Moria", "Plundering Goblins"];
+         assert.equal(detail.encounter_sets.length, encounter_sets.length, "encounter_sets.length");
+         var i;
+         for (i = 0; i < encounter_sets.length; i++)
+         {
+            assert.equal(detail.encounter_sets[i], encounter_sets[i], "encounter_sets[" + i + "]");
+         }
+
+         // console.log("Quest detail B: " + JSON.stringify(details[1]));
+         detail = details[1];
+         assert.equal(detail.pack_name, "Khazad-d&#251;m", "pack_name");
+         assert.equal(detail.encounter_set, "Flight from Moria", "encounter_set");
+         assert.equal(detail.type_code, "quest", "type_code");
+         assert.equal(detail.type_name, "Quest", "type_name");
+         assert.equal(detail.position, 70, "position");
+         assert.equal(detail.name, "Heading Down", "name");
+         assert.equal(detail.is_unique, undefined, "is_unique");
+         assert.equal(detail.sequence, "2B", "sequence");
+         assert.equal(detail.traits, undefined, "traits");
+         assert.equal(detail.text, "<b>Forced:</b> If Heading Up is in the player's victory display at the end of any quest phase, shuffle Heading Up back into the quest deck.<br/><b>Players may bypass this quest card at the end of the combat phase.", "text");
+         assert.equal(detail.shadow, undefined, "shadow");
+         // flavor missing
+         assert.equal(detail.engagement_cost, undefined, "engagement_cost");
+         assert.equal(detail.threat, undefined, "threat");
+         assert.equal(detail.attack, undefined, "attack");
+         assert.equal(detail.defense, undefined, "defense");
+         assert.equal(detail.hit_points, undefined, "hit_points");
+         assert.equal(detail.quest_points, 5, "quest_points");
+         assert.equal(detail.victory, undefined, "victory");
+         assert.equal(detail.quantity_easy, 1, "quantity_easy");
+         assert.equal(detail.quantity, 0, "quantity");
+         assert.equal(detail.image, "quest-card/Flight from Moria/Search-for-an-Exit-Heading-Down-2B.jpg", "image");
+
+         assert.equal(detail.encounter_sets.length, encounter_sets.length, "encounter_sets.length");
+         for (i = 0; i < encounter_sets.length; i++)
+         {
+            assert.equal(detail.encounter_sets[i], encounter_sets[i], "encounter_sets[" + i + "]");
+         }
+
+         done();
+      };
+
+      // Run.
+      var done = assert.async();
+      HoBDetailFetcher.fetch(file, callback);
+   });
+
    QUnit.test("fetch() Treachery Signs of Conflict", function(assert)
    {
       // Setup.
@@ -148,7 +313,6 @@ define(["qunit", "js/HoBDetailFetcher"], function(QUnit, HoBDetailFetcher)
          // console.log("Treachery detail: " + JSON.stringify(detail));
 
          assert.equal(detail.pack_name, "Khazad-d&#251;m", "pack_name");
-         assert.equal(detail.scenario_name, undefined, "scenario_name");
          assert.equal(detail.encounter_set, "Into the Pit", "encounter_set");
          assert.equal(detail.type_code, "treachery", "type_code");
          assert.equal(detail.type_name, "Treachery", "type_name");
