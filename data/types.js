87f09d7fc6eec68a94cf936228a4e6b981a2a45c
jmthompson2015 <softwaredev@jeffreythompson.net>
2018-03-08 10:40:22 -0700

    Added a JSON file of card types.

---
+++ data/types.js
@@ -0,0 +1,50 @@
+[
+   {
+      "type_code": "ally",
+      "type_name": "Ally"
+   },
+   {
+      "type_code": "attachment",
+      "type_name": "Attachment"
+   },
+   {
+      "type_code": "encounter-side-quest",
+      "type_name": "Encounter Side Quest"
+   },
+   {
+      "type_code": "enemy",
+      "type_name": "Enemy"
+   },
+   {
+      "type_code": "event",
+      "type_name": "Event"
+   },
+   {
+      "type_code": "hero",
+      "type_name": "Hero"
+   },
+   {
+      "type_code": "location",
+      "type_name": "Location"
+   },
+   {
+      "type_code": "objective",
+      "type_name": "Objective"
+   },
+   {
+      "type_code": "player-side-quest",
+      "type_name": "Player Side Quest"
+   },
+   {
+      "type_code": "quest",
+      "type_name": "Quest"
+   },
+   {
+      "type_code": "treachery",
+      "type_name": "Treachery"
+   },
+   {
+      "type_code": "treasure",
+      "type_name": "Treasure"
+   }
+]
