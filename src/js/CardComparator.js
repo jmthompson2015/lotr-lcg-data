e10a7895b45aae185072c558d81e714154cbed69
jmthompson2015 <softwaredev@jeffreythompson.net>
2018-03-14 10:13:44 -0600

    Sorted the saga cards to the bottom.

--- src/js/CardComparator.js
+++ src/js/CardComparator.js
@@ -4,10 +4,6 @@ define(["js/Pack"], function(Pack)
 {
    var CardComparator = function(a, b)
    {
-      // var a_pack_name = a.pack_name;
-      // var b_pack_name = b.pack_name;
-      //
-      // var answer = compare(a_pack_name, b_pack_name);
       var a_pack = Pack.findByName(a.pack_name);
       var b_pack = Pack.findByName(b.pack_name);
 
@@ -25,11 +21,6 @@ define(["js/Pack"], function(Pack)
 
       return answer;
    };
-
-   // function compare(a, b)
-   // {
-   //    return (a === b ? 0 : (a > b ? 1 : -1));
-   // }
 
    return CardComparator;
 });
